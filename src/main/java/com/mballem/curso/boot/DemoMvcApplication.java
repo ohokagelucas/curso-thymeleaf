c5dbc39ae87e8aff89ef287c18211e49516e4b0d
unknown <lucas.mendonca_@hotmail.com>
2021-07-15 20:29:18 -0300

    correção de erro de não execução

--- src/main/java/com/mballem/curso/boot/DemoMvcApplication.java
+++ src/main/java/com/mballem/curso/boot/DemoMvcApplication.java
@@ -1,4 +1,4 @@
-package com.mballem.curso.boot.com;
+package com.mballem.curso.boot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
