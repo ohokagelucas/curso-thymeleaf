7c68db4e0546fabe9d6fb00d49998044ada32369
unknown <lucas.mendonca_@hotmail.com>
2021-07-19 14:58:26 -0300

    inserção das funções de FUNCIONARIO

--- src/main/java/com/mballem/curso/boot/controller/FuncionarioController.java
+++ src/main/java/com/mballem/curso/boot/controller/FuncionarioController.java
@@ -1,20 +1,56 @@
 package com.mballem.curso.boot.controller;
 
+import java.util.*;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mballem.curso.boot.domain.*;
+import com.mballem.curso.boot.service.*;
+
 
 @Controller
 @RequestMapping("/funcionarios")
 public class FuncionarioController {
 
+	@Autowired
+	private FuncionarioService funcionarioService;
+	@Autowired
+	private CargoService cargoService;
+	
 	@GetMapping ("/cadastrar")
-	public String cadastrar() {
+	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
 
 	@GetMapping ("/listar")
-	public String listar() {
+	public String listar(ModelMap model) {
+		model.addAttribute("funcionarios", funcionarioService.buscarTodos());
 		return "/funcionario/lista";
+	}
+	
+	@PostMapping("/salvar")
+	public String salvar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.salvar(funcionario);
+		attr.addFlashAttribute("success","Funcionário inserido com sucesso.");
+		return "redirect:/funcionarios/cadastrar";
+	}
+	
+	@ModelAttribute("cargos")
+	public List<Cargo> getCargos() {
+		return cargoService.buscarTodos();	
+	}
+	
+	@ModelAttribute("ufs")
+	public UF[] getUFs() {
+		return UF.values();
+		
 	}
 }
--- src/main/java/com/mballem/curso/boot/domain/Funcionario.java
+++ src/main/java/com/mballem/curso/boot/domain/Funcionario.java
@@ -1,9 +1,17 @@
 package com.mballem.curso.boot.domain;
 
 import java.math.BigDecimal;
+
 import java.time.LocalDate;
 
 import javax.persistence.*;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+import org.springframework.format.annotation.NumberFormat;
+import org.springframework.format.annotation.NumberFormat.Style;
+
+
 
 @SuppressWarnings("serial")
 @Entity
@@ -13,12 +21,15 @@ public class Funcionario extends AbstractEntity<Long> {
 	@Column(nullable = false, unique = true)
 	private String nome;
 	
+	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
 	
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name= "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
 	
+	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 	
