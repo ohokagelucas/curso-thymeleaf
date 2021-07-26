3fb6d09b6fdb1b627a64424bb9700385815a5f18
unknown <lucas.mendonca_@hotmail.com>
2021-07-26 14:38:15 -0300

    validação mensagens de erro

--- src/main/java/com/mballem/curso/boot/domain/Funcionario.java
+++ src/main/java/com/mballem/curso/boot/domain/Funcionario.java
@@ -40,7 +40,7 @@ public class Funcionario extends AbstractEntity<Long> {
 	private LocalDate dataEntrada;
 	
 	@DateTimeFormat(iso = ISO.DATE)
-	@Column(name = "data_saida", nullable = false, columnDefinition = "DATE")
+	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 	
 	@Valid
